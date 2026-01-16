d10f9f37d1496626e50616c5157cd9eacbabaad0
Akila Madhushanka (KilerZack) <developer.akila5@gmail.com>
2026-01-16 18:26:22 +0530

    feat: Add Firebase configuration placeholders
    Adds placeholder values for Firebase configuration constants in `firebase.ts`. This allows for easier integration by providing clear locations for users to input their specific project credentials.

--- firebase.ts
+++ firebase.ts
@@ -4,6 +4,13 @@ import { getFirestore, collection, getDocs, setDoc, doc, deleteDoc } from "https
 import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
+  messagingSenderId: "YOUR_MSG_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MESURMENT_ID"
 };
 
 // Check if the user has actually filled in their config
